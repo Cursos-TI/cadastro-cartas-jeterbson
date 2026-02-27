b2a168894862a89bbadbd70eaf960df42b5d922b
Jeterbson <jeterbsonwender@gmail.com>
2026-02-27 01:27:00 +0000

    codico

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,23 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+ int Carta = 1;
+ char Estado = 'A';
+ char Codigo[10] = "A01";
+ char Cidade[50] = "Alvorada d' Oeste";
+ int Populacao = 13117;
+ float Area = 302919;
+ float PIB = 2553982;
+ int Turistico = 2;
 
+ printf("Carta %d\n", Carta);
+ printf("Estado: %c\n", Estado);
+ printf("Codigo: %s\n", Codigo);
+ printf("Nome da Cidade: %s\n", Cidade);
+ printf("Polupação: %d\n", Populacao);
+ printf("Area: %.2fkm²\n", Area);
+ printf("PIB: %.2f vinte e cimco mil reais\n", PIB);
+ printf("Numero de Pontos Turisticos: %i\n", Turistico);
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
