48889925f0d047523c6d88bef27029aff541203d
Jeterbson <jeterbsonwender@gmail.com>
2026-03-01 02:04:25 +0000

    N.Aventu. add no códico Dencid. e PibP. carta1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,23 +6,26 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  int Carta = 2;
-  char Estado = 'A';
-  char Código[10] = "A02";
-  char Cidade[50] = "Ji-Parana";
-  int População = 139359;
-  float Área = 6896.65;
-  float PIB = 5800000000;
-  int Turístico = 4;
+ int Carta = 1;
+ char Estado = 'A';
+ char Codigo[10] = "A01";
+ char Cidade[50] = "Alvorada d' Oeste";
+ int População = 13117;
+ float Área = 3029.19;
+ float PIB = 2553982;
+ int Turistico = 2;
 
-  printf("Carta %d\n", Carta);
-  printf("Estado: %c\n", Estado);
-  printf("Código: %s\n", Código);
-  printf("Nome da Cidade: %s\n", Cidade);
-  printf("Polupação: %d\n", População);
-  printf("Área: %.2fkm²\n", Área);
-  printf("PIB: %.2f bilhões\n", PIB);
-  printf("Número de Pontos Turísticos: %i\n", Turístico);
+ printf("Carta %d\n", Carta);
+ printf("Estado: %c\n", Estado);
+ printf("Codigo: %s\n", Codigo);
+ printf("Nome da Cidade: %s\n", Cidade);
+ printf("Polupação: %d\n", População);
+ printf("Area: %.2fkm²\n", Área);
+ printf("PIB: %.2f vinte e cimco mil reais\n", PIB);
+ printf("Numero de Pontos Turisticos: %i\n", Turistico);
+  // Área para entrada de dados
+
+  // Área para exibição dos dados da cidade
 
   float Dencidade;
   float PIPperCapito;
