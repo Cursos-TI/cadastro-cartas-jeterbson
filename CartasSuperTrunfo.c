4b40a4de704646d60074574bc37b35ff52060d8c
Jeterbson <jeterbsonwender@gmail.com>
2026-03-01 01:43:00 +0000

    Nivel Aventu. add no códico Dencid. e PIBpercap.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,26 +6,36 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
- int Carta = 2;
- char Estado = 'A';
- char Código[10] = "A02";
- char Cidade[50] = "Ji-Parana";
- int População = 139359;
- float Área = 6896.65;
- float PIB = 5.8;
- int Turístico = 4;
+   int Carta = 2;
+    char Estado = 'A';
+     char Código[10] = "A02";
+      char Cidade[50] = "Ji-Parana";
+       int População = 139359;
+        float Área = 6896.65;
+         float PIB = 5800000000;
+          int Turístico = 4;
 
- printf("Carta %d\n", Carta);
- printf("Estado: %c\n", Estado);
- printf("Código: %s\n", Código);
- printf("Nome da Cidade: %s\n", Cidade);
- printf("Polupação: %d\n", População);
- printf("Área: %.2fkm²\n", Área);
- printf("PIB: %.1f bilhões\n", PIB);
- printf("Número de Pontos Turísticos: %i\n", Turístico);
-  // Área para entrada de dados
+           printf("Carta %d\n", Carta);
+            printf("Estado: %c\n", Estado);
+             printf("Código: %s\n", Código);
+              printf("Nome da Cidade: %s\n", Cidade);
+               printf("Polupação: %d\n", População);
+                printf("Área: %.2fkm²\n", Área);
+                 printf("PIB: %.2f bilhões\n", PIB);
+                  printf("Número de Pontos Turísticos: %i\n", Turístico);
 
-  // Área para exibição dos dados da cidade
+                   float Dencidade;
+ float PIPperCapito;
 
-return 0;
-} 
+ Dencidade = (População / Área);
+ printf("Dencidade Populacional: %.2f hab/km²\n", Dencidade);
+ 
+ PIPperCapito = (PIB / População);
+ printf("PIB per Capita: %.2f reais\n", PIPperCapito);
+
+                    // Área para entrada de dados
+
+                      // Área para exibição dos dados da cidade
+
+                      return 0;
+                      } 
